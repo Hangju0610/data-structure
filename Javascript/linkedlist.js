f93a7de35801e1d41d2543558f1e56bedeb73443
hangju0610 <phj950610@naver.com>
2023-11-02 23:00:20 +0900

    Feat linkedlist 구현 중

--- Javascript/linkedlist.js
+++ Javascript/linkedlist.js
@@ -41,6 +41,20 @@ class LinkedList {
     tail.next = newNode;
     this.size++;
   }
+  deleteEnd() {
+    if (this.size === 1) {
+      this.head = null;
+      this.size--;
+    }
+    let current = this.head;
+    let previous;
+    while (current.next === null) {
+      previous = current;
+      current = current.next;
+    }
+    previous.next = null;
+    this.size--;
+  }
 
   // 특정 위치에 노드 추가하기
   insertAt(data, index = 0) {
@@ -69,9 +83,39 @@ class LinkedList {
     this.size++;
   }
 
+  deleteAt(index) {
+    // 만약 index 값이 size보다 크다면
+    if (index > this.size)
+      return console.log("총 노드보다 큰 값을 입력했습니다.");
+
+    // 매개변수 index와 내가 센 idx가 같을 때 삽입하는 용도로 사용
+    let idx = 0;
+    // 특정 위치에 노드를 제거하려면 먼저
+    // 그 전과 그 후의 데이터가 필요하다.
+    let current = this.head;
+    let previous;
+    // 입력값까지 idx가 도달 할 수 있도록 반복문 진행
+    while (idx < index) {
+      previous = current;
+      current = current.next;
+      idx++;
+    }
+    previous.next = current.next;
+    this.size--;
+  }
+
   // Node 개수 출력
   printNode() {
     return this.size;
+  }
+  find(index) {
+    if (this.size === 0) return null;
+    if (index > this.size) return null;
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
   }
 
   // 특정 데이터를 가지고 있는 Node의 인덱스를 찾기
@@ -102,4 +146,4 @@ a.insertAt(4, 2);
 console.log(a.printNode());
 console.log(a.findIndex(4));
 a.pop();
-console.log(a);
+console.log(a.find(2));
