5ca138ea07015dd2b108cfb2b8cc7db3d7e43d5f
hangju0610 <phj950610@naver.com>
2023-11-15 23:00:19 +0900

    Feat: Add adjacency_list

--- Javascript/Adjacency_Matrix.js
+++ Javascript/Adjacency_Matrix.js
@@ -3,6 +3,14 @@ function Matrix(v) {
   // 따라서 2차원 배열을 만들 경우 이렇게 만들어야 한다.
   const a = Array.from(Array(10), () => new Array(10).fill(0));
   const visited = Array.from(Array(10).fill(0));
-  a[1][2] = 1; a[1][3] = 1; a[3][4] = 1;
-  a[2][1] = 1; a[3][1] = 1; a[4][1] = 1;
-} 
+  a[1][2] = 1;
+  a[1][3] = 1;
+  a[3][4] = 1;
+  a[2][1] = 1;
+  a[3][1] = 1;
+  a[4][1] = 1;
+  console.log(visited);
+  console.log(a);
+}
+
+Matrix(3);
